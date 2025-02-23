d8455835580b57a2065b40b6360cc87e1ebba748
code-leech <upperint2011@gmail.com>
2025-02-23 10:59:21 +0000

    Finished formatting C code

--- src/simpleapplication-window.c
+++ src/simpleapplication-window.c
@@ -24,10 +24,10 @@
 
 struct _SimpleapplicationWindow
 {
-	AdwApplicationWindow  parent_instance;
+  AdwApplicationWindow  parent_instance;
 
-	/* Template widgets */
-	GtkButton            *button;
+  /* Template widgets */
+  GtkButton            *button;
 };
 
 G_DEFINE_FINAL_TYPE (SimpleapplicationWindow, simpleapplication_window, ADW_TYPE_APPLICATION_WINDOW)
@@ -35,48 +35,48 @@ G_DEFINE_FINAL_TYPE (SimpleapplicationWindow, simpleapplication_window, ADW_TYPE
 static void
 simpleapplication_window_class_init (SimpleapplicationWindowClass *klass)
 {
-	GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
+  GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
 
-	gtk_widget_class_set_template_from_resource (widget_class, "/org/self/SimpleApplication/simpleapplication-window.ui");
-	gtk_widget_class_bind_template_child (widget_class, SimpleapplicationWindow, button);
+  gtk_widget_class_set_template_from_resource (widget_class, "/org/self/SimpleApplication/simpleapplication-window.ui");
+  gtk_widget_class_bind_template_child (widget_class, SimpleapplicationWindow, button);
 }
 
 static void
 simpleapplication_button_finish (gpointer user_data)
 {
-        static const char *button_css[] = {"suggested-action", "title-3", "pill", NULL};
-        GtkButton *button = user_data;
-        gtk_button_set_label (button, "Click me!");
-        gtk_widget_set_css_classes (GTK_WIDGET (button), button_css);
-        gtk_widget_set_sensitive (GTK_WIDGET (button), true);
+  static const char *button_css[] = {"suggested-action", "title-3", "pill", NULL};
+  GtkButton *button = user_data;
+  gtk_button_set_label (button, "Click me!");
+  gtk_widget_set_css_classes (GTK_WIDGET (button), button_css);
+  gtk_widget_set_sensitive (GTK_WIDGET (button), true);
 }
 
 static void
 simpleapplication_button_start (GtkButton *button,
                                 gpointer user_data)
 {
-        static const char *button_css[] = {"destructive-action", "title-3", "pill", NULL};
-        gtk_widget_set_sensitive (GTK_WIDGET (button), false);
-        gtk_button_set_label (button, "Clicked!");
-        gtk_widget_set_css_classes (GTK_WIDGET (button), button_css);
-        g_timeout_add_seconds_once (2, simpleapplication_button_finish, button);
+  static const char *button_css[] = {"destructive-action", "title-3", "pill", NULL};
+  gtk_widget_set_sensitive (GTK_WIDGET (button), false);
+  gtk_button_set_label (button, "Clicked!");
+  gtk_widget_set_css_classes (GTK_WIDGET (button), button_css);
+  g_timeout_add_seconds_once (2, simpleapplication_button_finish, button);
 }
 
 static void
 simpleapplication_setup (gpointer user_data)
 {
-        SimpleapplicationWindow *self = user_data;
-        AdwStyleManager *style = ADW_STYLE_MANAGER (adw_style_manager_get_default ());
+  SimpleapplicationWindow *self = user_data;
+  AdwStyleManager *style = ADW_STYLE_MANAGER (adw_style_manager_get_default ());
 
-        g_signal_connect (self->button, "clicked", G_CALLBACK (simpleapplication_button_start), NULL);
+  g_signal_connect (self->button, "clicked", G_CALLBACK (simpleapplication_button_start), NULL);
 
-        adw_style_manager_set_color_scheme (style, ADW_COLOR_SCHEME_FORCE_DARK);
+  adw_style_manager_set_color_scheme (style, ADW_COLOR_SCHEME_FORCE_DARK);
 
 }
 
 static void
 simpleapplication_window_init (SimpleapplicationWindow *self)
 {
-	gtk_widget_init_template (GTK_WIDGET (self));
-        g_idle_add_once (simpleapplication_setup, self);
+  gtk_widget_init_template (GTK_WIDGET (self));
+  g_idle_add_once (simpleapplication_setup, self);
 }
