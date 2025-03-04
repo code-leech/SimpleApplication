bff238f151603605ca89bc3028351db88e4e5a7f
code-leech <upperint2011@gmail.com>
2025-03-04 18:59:15 +0000

    Refined menu looks

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,4 @@
-pub static VERSION: &str = "0.1.0";
+pub static VERSION: &str = "0.2.0";
 pub static GETTEXT_PACKAGE: &str = "simpleapplication";
 pub static LOCALEDIR: &str = "/app/share/locale";
 pub static PKGDATADIR: &str = "/app/share/simpleapplication";
